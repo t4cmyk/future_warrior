b246400df64cecd0822486345675a0551fafb8bc
Michael Schneider <->
2021-04-16 20:12:30 +0200

    more db tables added

--- server/migrations/001-initial.sql
+++ server/migrations/001-initial.sql
@@ -2,7 +2,7 @@
 -- Up
 --------------------------------------------------------------------------------
 
-CREATE TABLE Player (
+CREATE TABLE players (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT NOT NULL,
 	password TEXT NOT NULL,
@@ -11,8 +11,46 @@ CREATE TABLE Player (
 	mail TEXT NOT NULL
 );
 
+CREATE TABLE missions (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL,
+	description TEXT NOT NULL,
+	imagePath TEXT NOT NULL,
+	score INTEGER NOT NULL,
+	sector TEXT NOT NULL,
+	advanced BOOL NOT NULL,
+	custom BOOL NOT NULL,
+	creator INTEGER NOT NULL 
+);
+
+CREATE TABLE teams (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL,
+  sector1 TEXT,
+  sector2 TEXT 
+);
+
+CREATE TABLE games (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	start DATE NOT NULL,
+	ending DATE NOT NULL
+);
+
+CREATE TABLE participates (
+  gameId INTEGER NOT NULL,
+  teamId INTEGER NOT NULL,
+  playerId INTEGER NOT NULL,
+  score INTEGER NOT NULL
+);
+
+
+
 --------------------------------------------------------------------------------
 -- Down
 --------------------------------------------------------------------------------
 
-DROP TABLE Player;
+DROP TABLE players;
+DROP TABLE missions;
+DROP TABLE teams;
+DROP TABLE games;
+DROP TABLE participates;
