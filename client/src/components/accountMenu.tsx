6b8035219b347df1fe4bc7fc2f20591612b6c43f
Jakob Nedlin <->
2021-04-18 20:57:53 +0200

    Updated accountMenu

--- client/src/components/accountMenu.tsx
+++ client/src/components/accountMenu.tsx
@@ -5,12 +5,20 @@ import { useLoginState } from "./hooks/loginState";
 export function AccountMenu(props: {}) {
   const loginState = useLoginState();
 
-  if (!loginState) return <></>;
+  if (!loginState)
+    return (
+      <div className="justify-content-end col-12 d-flex">
+        {`Nicht angemeldet\u00A0`}
+        <a href="#/Register">Registrieren</a>/<a href="#/Login">Einloggen</a>
+      </div>
+    );
 
   return (
     <div className="justify-content-end col-12 d-flex">
-      Angemeldet als {loginState.username}{" "}
-      <span onClick={logoutPlayer}> (ausloggen)</span>
+      {`Angemeldet als ${loginState.username}\u00A0`}
+      <a href="#" onClick={logoutPlayer}>
+        (ausloggen)
+      </a>
     </div>
   );
 }
