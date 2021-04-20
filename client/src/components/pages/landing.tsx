089ee9740a6a0116c7a6412f35875be618ee0327
Michael Schneider <->
2021-04-20 21:06:05 +0200

    updated Landing (WIP)

--- client/src/components/pages/landing.tsx
+++ client/src/components/pages/landing.tsx
@@ -1,18 +1,36 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 
 export function Landing() {
   return (
     <>
-      <h1>Future Warrior - Performativ in die Zukunft!</h1>
       <br />
-      <img src="placeholder.jpg" />
-      <br />
-      <a href="/#/Register">
-        <button>Registrieren</button>
-      </a>
-      <a href="/#/Login">
-        <button>Einloggen</button>
-      </a>
+      <Container>
+        <img src="placeholder.jpg" />
+        <br />
+        <h2>Spiele, um mit deinem Team eure Parallelwelt aufzubauen.</h2>
+        <br />
+        <img src="img/utopia/planet.png" height="100" width="100" />
+        <br />
+        <h2>Erledige Missionen, um Punkte für den Aufbau zu generieren.</h2>
+        <br />
+        <h3>[Bild Missionskarte]</h3>
+        <br />
+        <h2>Entscheidet, auf welche Sektoren ihr euch spezialisieren wollt.</h2>
+        <br />
+        <img src="img/missions/diet.jpg" height="100" width="100" />
+        <img src="img/missions/energy.jpg" height="100" width="100" />
+        <img src="img/missions/household.jpg" height="100" width="100" />
+        <img src="img/missions/mobility.jpg" height="100" width="100" />
+        <img src="img/missions/social.jpg" height="100" width="100" />
+        <br />
+        <a href="/#/Register">
+          <button>Registrieren</button>
+        </a>
+        <a href="/#/Login">
+          <button>Einloggen</button>
+        </a>
+      </Container>
     </>
   );
 }
