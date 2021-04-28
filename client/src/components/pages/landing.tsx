26bdb3b9498bfbcc7699e9ab2e45747443fdb9df
Michael Schneider <->
2021-04-28 20:54:19 +0200

    trailer video added to landing and style fixes

--- client/src/components/pages/landing.tsx
+++ client/src/components/pages/landing.tsx
@@ -5,32 +5,48 @@ export function Landing() {
   return (
     <>
       <br />
-      <Container>
-        <img src="placeholder.jpg" />
-        <br />
-        <h2>Spiele, um mit deinem Team eure Parallelwelt aufzubauen.</h2>
-        <br />
-        <img src="img/utopia/planet.png" height="100" width="100" />
-        <br />
-        <h2>Erledige Missionen, um Punkte für den Aufbau zu generieren.</h2>
-        <br />
-        <h3>[Bild Missionskarte]</h3>
-        <br />
-        <h2>Entscheidet, auf welche Sektoren ihr euch spezialisieren wollt.</h2>
-        <br />
-        <img src="img/missions/diet.png" height="100" width="100" />
-        <img src="img/missions/energy.png" height="100" width="100" />
-        <img src="img/missions/household.png" height="100" width="100" />
-        <img src="img/missions/mobility.png" height="100" width="100" />
-        <img src="img/missions/social.png" height="100" width="100" />
-        <br />
-        <a href="/#/Register">
-          <button>Registrieren</button>
-        </a>
-        <a href="/#/Login">
-          <button>Einloggen</button>
-        </a>
+
+      <video
+        width="800"
+        height="450"
+        controls
+        src="trailer.mp4"
+        className="center"
+      >
+      </video>
+      <br />
+ 
+      <img src="img/utopia/planet.png" className="center" />
+      <h2>
+        Spiele, um mit deinem Team eure Parallelwelt aufzubauen.
+      </h2>
+      <br />
+      <br />
+      <h3>[Bild Missionskarte]</h3>
+      <br />
+      <h2 className="center">
+        Erledige Missionen, um Punkte für den Aufbau zu generieren.
+      </h2>
+      <br />
+      
+      <Container className="justify-content-center col-12 d-flex">
+        <img src="img/missions/diet.png" height="200" width="200" />
+        <img src="img/missions/energy.png" height="200" width="200" />
+        <img src="img/missions/household.png" height="200" width="200" />
+        <img src="img/missions/mobility.png" height="200" width="200" />
+        <img src="img/missions/social.png" height="200" width="200" />
       </Container>
+      <br />
+      <h2>
+        Entscheidet, auf welche Sektoren ihr euch spezialisieren wollt.
+      </h2>
+      <br />
+      <a href="/#/Register">
+        <button>Registrieren</button>
+      </a>
+      <a href="/#/Login">
+        <button>Einloggen</button>
+      </a>
     </>
   );
 }
