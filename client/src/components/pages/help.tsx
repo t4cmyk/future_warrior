37e1c7b70b3165cccde42b7718fa860631acd683
Michael Schneider <->
2021-04-28 20:05:03 +0200

    help page - style changed and content added

--- client/src/components/pages/help.tsx
+++ client/src/components/pages/help.tsx
@@ -2,8 +2,18 @@ import React from "react";
 import { Accordion, Card, Container } from "react-bootstrap";
 
 let qaPairs = [
-  ["Frage 1", "Antwort 1"],
-  ["Frage 2", "Antwort 2"],
+  [
+    "Was kann ich tun, wenn ich eine bereits gespeicherte Eingabe rückgängig machen will?",
+    "Nimm über das untenstehende Formular Kontakt mit der Spielleitung auf und wir schauen, was wir in deinem Fall tun können.",
+  ],
+  [
+    "Was mache ich, wenn ich während der Spielphase merke, dass ich aus persönlichen Gründen nicht weiterspielen kann?",
+    "In diesem Fall melde dich bitte zuerst über das untenstehende Kontaktformular an die Spielleitung, bevor du dein Team einweihst. Wir besprechen gemeinsam, wie wir einen reibungslosen Übergang gestalten können.",
+  ],
+  [
+    "Was mache ich, wenn ich beobachte, dass eine Person nicht regelkonform spielt?",
+    "Verdeutliche dir zuallererst, dass in erster Linie die Person, welche die Regeln verletzt, selbst einen Nachteil davon hat. Diese Person beraubt sich selbst der Erfahrungen, welche sie durch das Spiel erlangen könnte.\n Da im Team gespielt wird, soll natürlich das restliche Team dadurch nicht beeinflusst werden. Daher melde bitte genau, was du beobachten konntest an die Spielleitung. Wir entscheiden ggf. zusammen, was in dem jeweiligen Fall passieren kann und soll. Nutze das untenstehende Kontaktformular, um deine Beobachtungen mitzuteilen.",
+  ],
 ];
 
 function Question(props: {
