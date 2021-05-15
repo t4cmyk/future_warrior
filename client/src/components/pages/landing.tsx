a84712897e2a9287f869f9d4ebdbe43d97fb4783
Michael Schneider <->
2021-05-15 22:08:53 +0200

    updated landing movie

--- client/src/components/pages/landing.tsx
+++ client/src/components/pages/landing.tsx
@@ -10,16 +10,18 @@ export function Landing() {
         width="800"
         height="450"
         controls
-        src="trailer.mp4"
+        src="this-is-mf.mp4"
         className="center"
-      >
-      </video>
+      ></video>
       <br />
- 
-      <img src="img/planet/edre.png" className="center" height="600px" width="600px" />
-      <h2>
-        Spiele, um mit deinem Team eure Parallelwelt aufzubauen.
-      </h2>
+
+      <img
+        src="img/planet/edre.png"
+        className="center"
+        height="600px"
+        width="600px"
+      />
+      <h2>Spiele, um mit deinem Team eure Parallelwelt aufzubauen.</h2>
       <br />
       <br />
       <h3>[Bild Missionskarte]</h3>
@@ -28,7 +30,7 @@ export function Landing() {
         Erledige Missionen, um Punkte für den Aufbau zu generieren.
       </h2>
       <br />
-      
+
       <Container className="justify-content-center col-12 d-flex">
         <img src="img/missions/diet.png" height="200" width="200" />
         <img src="img/missions/energy.png" height="200" width="200" />
@@ -37,9 +39,7 @@ export function Landing() {
         <img src="img/missions/social.png" height="200" width="200" />
       </Container>
       <br />
-      <h2>
-        Entscheidet, auf welche Sektoren ihr euch spezialisieren wollt.
-      </h2>
+      <h2>Entscheidet, auf welche Sektoren ihr euch spezialisieren wollt.</h2>
       <br />
     </>
   );
