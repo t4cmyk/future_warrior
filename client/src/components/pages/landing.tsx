1cf8df4197b03e6dd05f7ec228f0250faed7bb6f
Michael Schneider <->
2021-05-17 23:56:18 +0200

    removed images from landing page

--- client/src/components/pages/landing.tsx
+++ client/src/components/pages/landing.tsx
@@ -14,33 +14,20 @@ export function Landing() {
         className="center"
       ></video>
       <br />
-
+      <br />
+      <h2>Spiele, um mit deinem Team eure Parallelwelt aufzubauen.</h2>
+      <br />
+      <h2 className="center">
+        Erledige Missionen, um Punkte für den Aufbau zu generieren.
+        </h2>
+        <br />
+      <h2>Entscheidet, auf welche Sektoren ihr euch spezialisieren wollt.</h2>
       <img
         src="/img/planet/edre.png"
         className="center"
         height="600px"
         width="600px"
       />
-      <h2>Spiele, um mit deinem Team eure Parallelwelt aufzubauen.</h2>
-      <br />
-      <br />
-      <h3>[Bild Missionskarte]</h3>
-      <br />
-      <h2 className="center">
-        Erledige Missionen, um Punkte für den Aufbau zu generieren.
-      </h2>
-      <br />
-
-      <Container className="justify-content-center col-12 d-flex">
-        <img src="/img/missions/diet.png" height="200" width="200" />
-        <img src="/img/missions/energy.png" height="200" width="200" />
-        <img src="/img/missions/household.png" height="200" width="200" />
-        <img src="/img/missions/mobility.png" height="200" width="200" />
-        <img src="/img/missions/social.png" height="200" width="200" />
-      </Container>
-      <br />
-      <h2>Entscheidet, auf welche Sektoren ihr euch spezialisieren wollt.</h2>
-      <br />
     </>
   );
 }
