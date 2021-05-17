763556a2d9bc61a5522f721f9f1f9ab5cb651cfa
Jakob Nedlin <->
2021-05-17 23:40:04 +0200

    Turned relative into absolute links

--- client/src/components/pages/landing.tsx
+++ client/src/components/pages/landing.tsx
@@ -10,13 +10,13 @@ export function Landing() {
         width="800"
         height="450"
         controls
-        src="this-is-mf.mp4"
+        src="/this-is-mf.mp4"
         className="center"
       ></video>
       <br />
 
       <img
-        src="img/planet/edre.png"
+        src="/img/planet/edre.png"
         className="center"
         height="600px"
         width="600px"
@@ -32,11 +32,11 @@ export function Landing() {
       <br />
 
       <Container className="justify-content-center col-12 d-flex">
-        <img src="img/missions/diet.png" height="200" width="200" />
-        <img src="img/missions/energy.png" height="200" width="200" />
-        <img src="img/missions/household.png" height="200" width="200" />
-        <img src="img/missions/mobility.png" height="200" width="200" />
-        <img src="img/missions/social.png" height="200" width="200" />
+        <img src="/img/missions/diet.png" height="200" width="200" />
+        <img src="/img/missions/energy.png" height="200" width="200" />
+        <img src="/img/missions/household.png" height="200" width="200" />
+        <img src="/img/missions/mobility.png" height="200" width="200" />
+        <img src="/img/missions/social.png" height="200" width="200" />
       </Container>
       <br />
       <h2>Entscheidet, auf welche Sektoren ihr euch spezialisieren wollt.</h2>
