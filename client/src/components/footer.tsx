317b8cc914f87f2b1693d81feb0b8bed2cb01038
Michael Schneider <->
2021-05-21 13:46:51 +0200

    added logos to the footer

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -1,13 +1,34 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export function Footer() {
   return (
-    <footer>
-      <hr />
-      <Link to="/Help/">Hilfe</Link> <Link to="/Privacy/">Datenschutz</Link>{" "}
-      <Link to="/Impressum/">Impressum</Link>{" "}
-      <Link to="/Contact/">Kontakt</Link>
-    </footer>
+    <>
+      <br />
+      <br />
+      <footer>
+        <div className="d-flex justify-content-around">
+          <div>
+            <img src="/img/logos/disPos.png" height="50" width="0" />
+            <img className="logo" src="/img/logos/disPos.png" />
+            {"   "}
+            <img className="logo" src="/img/logos/spHi.jpg" />
+          </div>
+          <div>
+            <Link to="/Help/">Hilfe</Link>{" "}
+            <Link to="/Privacy/">Datenschutz</Link>{" "}
+            <Link to="/Impressum/">Impressum</Link>{" "}
+            <Link to="/Contact/">Kontakt</Link>
+          </div>
+          <div>
+            <img src="/img/logos/disPos.png" height="50" width="0" />
+            <img className="logo" src="/img/logos/studOst.png" />
+            {"   "}
+            <img className="logo" src="/img/logos/stuPa.jpg" />
+          </div>
+        </div>
+      </footer>
+    </>
   );
 }
