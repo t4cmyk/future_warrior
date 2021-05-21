9df213e03b6327226c155334ad5997788e057a4a
Jakob Nedlin <->
2021-05-21 14:49:12 +0200

    Link to main page when logged in

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { AccountMenu } from "./accountMenu";
+import { useLoginState } from "./hooks/loginState";
 
 export function Header() {
+  const loginState = useLoginState();
+  const linkTo = loginState ? "/Main" : "/Landing";
+
   return (
     <header>
       <div className="justify-content-between col-12 d-flex">
-        <Link to="/Landing">
+        <Link to={linkTo}>
           <img width="288" height="100" src="/img/missionfuture.png" />
         </Link>
         <AccountMenu />
