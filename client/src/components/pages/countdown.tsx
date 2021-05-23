4dabdc541fad4404f9c3307e20cc92d3f237c842
Michael Schneider <->
2021-05-23 21:01:54 +0200

    countdown - under construction message added

--- client/src/components/pages/countdown.tsx
+++ client/src/components/pages/countdown.tsx
@@ -1,5 +1,5 @@
 import React, { useRef } from "react";
-import { Button } from "react-bootstrap";
+import { Alert, Button, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 //double-digits
@@ -12,7 +12,7 @@ export function Countdown() {
   const timerRef = useRef<HTMLHeadingElement>(null);
   const captionRef = useRef<HTMLHeadingElement>(null);
   let time = "Bald geht's los!";
-  let gameStart = new Date("2021-05-23T20:00:00");
+  let gameStart = new Date("2021-05-25T04:00:00");
   let clock = setInterval(function () {
     let dist = gameStart.getTime() - new Date().getTime();
     let days = Math.floor(dist / (1000 * 60 * 60 * 24));
@@ -22,7 +22,8 @@ export function Countdown() {
     time = days + " Tage " + dd(hours) + ":" + dd(minutes) + ":" + dd(seconds);
     if (dist <= 0) {
       clearInterval(clock);
-      time = "Das Spiel beginnt in wenigen Augenblicken!";
+      time =
+        "Wenn das Spiel jetzt nicht begonnen hat, dann haben wir noch mehr technische Schwierigkeiten :(";
       captionRef.current.innerText = "";
     }
     if (timerRef.current == null) {
@@ -35,6 +36,23 @@ export function Countdown() {
   return (
     <>
       <br />
+      <Container>
+        <Alert variant="warning">
+          Schön, dass du dich angemeldet hast!
+          <br />
+          <br />
+          Was wäre ein Projekt ohne Pannen? Leider haben wir eine Verzögerung
+          beim Spielstart. Die K.I. muss noch die letzte Testphase durchlaufen.
+          <br />
+          <br />
+          Das Spiel startet am Dienstag, den 25.05. und endet am 08.06. Der
+          Countdown zum Spielstart endet um 4 Uhr. Das liegt daran, dass zu
+          dieser Zeit immer die Missionen für den neuen Tag gezogen werden. Ihr
+          könnt ganz entspannt und individuell am Dienstag starten, wann ihr
+          wollt.
+          <br />
+        </Alert>
+      </Container>
       <h1 ref={timerRef}>? Tage ??:??:??</h1>
       <br />
       <h3 ref={captionRef}>bis zum Spielbeginn</h3>
