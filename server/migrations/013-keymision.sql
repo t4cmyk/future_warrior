c1705b6c8fc8b75d45c61da570c5c8fd004106ee
Jakob Nedlin <->
2021-05-30 19:02:31 +0200

    Added key mission entry

---
+++ server/migrations/013-keymision.sql
@@ -0,0 +1,7 @@
+--------------------------------------------------------------------------------
+-- Up
+--------------------------------------------------------------------------------
+INSERT INTO missions (name, description, score, sector, creatorId) VALUES ('Schlüssel-Mission','Das ist eure gemeinsame Team-Mission, die ihr absolvieren müsst, um das letzte Level abzuschließen. Eine detailierte Beschreibung findet ihr unterhalb der Missionsauswahl.' , 20, 'Schlüsselereignis', -1);
+--------------------------------------------------------------------------------
+-- Down
+--------------------------------------------------------------------------------
